6cdde8dd5781d7b06d288b545e65649644100fc7
Ibai <ibai.vaqueroro@elorrieta-errekamari.com>
2024-03-08 13:42:31 +0100

    prueba pull

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -38,7 +38,7 @@ const Header = () => {
                             ></NavLink>
                         </li>
                     ))}
-                </ul>
+                </ul> 
             </div>
             <div>
                 <ul className="flex space-x-4">
