bb139681ac93c87af53fa1e46519c5ab92dc6ffb
aroa <aroa.rodriguez@ikasle.egibide.org>
2024-03-11 10:46:14 +0100

    Product, Category, Review and Order mcr, factory and seeder

---
+++ app/Models/Order.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'imageFiles', 'numProducts', 'numPerspective', '3DModel', 'deadline', 'additionalInfo'
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'imageFiles' => 'array',
+        '3DModel' => 'array',
+        'deadline' => 'datetime',
+    ];
+}
