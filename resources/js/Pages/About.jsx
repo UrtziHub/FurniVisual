e6428aeadbf1a4836abb765ca334b1d463cf9f25
Ibai <ibai.vaqueroro@elorrieta-errekamari.com>
2024-03-11 13:59:08 +0100

    about us hecho

--- resources/js/Pages/About.jsx
+++ resources/js/Pages/About.jsx
@@ -1,9 +1,36 @@
 import PageLayout from "@/Layouts/PageLayout";
 
-export default function About({auth}) {
-    return (
-        <PageLayout user={auth.user} >
-            <h1 className="text-xl">About us <a href="/" className="underline">go home</a></h1>
-        </PageLayout>
-    );
+export default function About({ auth }) {
+  return (
+    <PageLayout user={auth.user}>
+      <h1 className="text-6xl font-bold mb-8 text-center">About Us</h1>
+      <div className="flex flex-col items-center gap-20 mx-20">
+        <div className="flex flex-row mx-8 rounded-lg bg-gray-100 ">
+          <div className="w-1/2  p-8 rounded-l-lg shadow-lg flex justify-center items-center">
+            <p className="text-lg text-gray-700">
+              Welcome to our Graphic Studio nestled in the picturesque city of Kraków. We specialize in turning your ideas into stunning visualizations, focusing on interiors, furniture, and equipment. Our team is dedicated to utilizing 3D modeling and animations to breathe life into pre-existing concepts.
+              At our studio, we are not artists in the traditional sense, nor do we consider ourselves designers or architects. Instead, we take pride in being meticulous craftsmen who transform ready-made projects provided by our clients into captivating visual stories. Our role is to elevate the materials handed to us, infusing them with life and vibrancy through our technical expertise.
+            </p>
+          </div>
+          <img src="images/image1.jpg" alt="Company Logo" className="w-1/2 rounded-r-lg shadow-lg" />
+        </div>
+        <div className="flex flex-row mx-8">
+          <img src="images/image2.jpg" alt="Company Logo" className="w-1/2 rounded-l-lg shadow-lg" />
+          <div className="w-1/2 bg-gray-100 p-8 rounded-r-lg shadow-lg flex justify-center items-center ">
+            <p className="text-lg text-gray-700">
+            Punctuality is our promise. We understand the importance of deadlines, and our commitment to delivering on time is unwavering. The quality of our work is a reflection of our dedication to excellence. Each visualization, animation, and project we undertake is executed with precision and an unwavering focus on high standards.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col  justify-center items-center bg-gray-100 mx-40 rounded-t-lg shadow-lg pb-10">
+          <h1 className="pt-10 text-3xl">Our Team</h1>
+          <p className="px-52 py-10 text-center text-lx">Our Graphic Design Team, although not traditional artists, is a group of skilled professionals dedicated to enhancing your projects. We take pride in our after-sales support, ensuring your satisfaction with the final result.
+             Step into our studio, where we don’t just work on projects; we transform them into visual masterpieces. At our Graphic Studio, your concepts find a home where technical prowess meets reliability, and client-provided materials evolve into captivating visual narratives. Welcome to a space where quality, punctuality, and client satisfaction are our guiding principles.</p>
+          <div className="w-{500px}">
+          <img src="images/group.png" alt="" className="rounded-b-lg"/>
+          </div>
+        </div>
+      </div>
+    </PageLayout>
+  );
 }
--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,42 +1,50 @@
-import { useEffect } from 'react';
-import Checkbox from '@/Components/Checkbox';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
-import Header from '@/Components/Header';
+import { useEffect } from "react";
+import Checkbox from "@/Components/Checkbox";
+import GuestLayout from "@/Layouts/GuestLayout";
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import PrimaryButton from "@/Components/PrimaryButton";
+import TextInput from "@/Components/TextInput";
+import { Head, Link, useForm } from "@inertiajs/react";
 
 export default function Login({ status, canResetPassword }) {
     const { data, setData, post, processing, errors, reset } = useForm({
-        email: '',
-        password: '',
-        remember: '',
+        email: "",
+        password: "",
+        remember: "",
     });
 
     useEffect(() => {
         return () => {
-            reset('password');
+            reset("password");
         };
     }, []);
 
     const handleOnChange = (event) => {
-        setData(event.target.name, event.target.type === 'checkbox' ? event.target.checked : event.target.value);
+        setData(
+            event.target.name,
+            event.target.type === "checkbox"
+                ? event.target.checked
+                : event.target.value
+        );
     };
 
     const submit = (e) => {
         e.preventDefault();
 
-        post(route('login'));
+        post(route("login"));
     };
 
     return (
         <GuestLayout>
             <Head title="Log in" />
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
+            {status && (
+                <div className="mb-4 font-medium text-sm text-green-600">
+                    {status}
+                </div>
+            )}
             <form onSubmit={submit}>
-                <div> 
+                <div>
                     <InputLabel htmlFor="email" value="Email" />
 
                     <TextInput
@@ -71,24 +79,38 @@ export default function Login({ status, canResetPassword }) {
 
                 <div className="block mt-4">
                     <label className="flex items-center">
-                        <Checkbox name="remember" value={data.remember} onChange={handleOnChange} />
-                        <span className="ml-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>
+                        <Checkbox
+                            name="remember"
+                            value={data.remember}
+                            onChange={handleOnChange}
+                        />
+                        <span className="ml-2 text-sm text-gray-600 dark:text-gray-400">
+                            Remember me
+                        </span>
                     </label>
                 </div>
 
                 <div className="flex items-center justify-end mt-4">
                     {canResetPassword && (
                         <Link
-                            href={route('password.request')}
+                            href={route("password.request")}
                             className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                         >
                             Forgot your password?
                         </Link>
                     )}
 
-                    <PrimaryButton className="ml-4" disabled={processing}>
+                    <PrimaryButton className="ml-4 mr-2" disabled={processing}>
                         Log in
                     </PrimaryButton>
+                    <Link
+                        className="     
+                            ml-4 inline-flex items-center px-4 py-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150
+                        "
+                        href={route("register")}
+                    >
+                        Sign up
+                    </Link>
                 </div>
             </form>
         </GuestLayout>
