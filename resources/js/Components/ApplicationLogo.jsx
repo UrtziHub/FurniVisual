e7e228c9fdc4f08a2cd51c583dc63f1f8ce635ea
UrtziHub <ulusarretaga22wg@ikzubirimanteo.com>
2024-03-12 15:55:31 +0100

    Login responsive

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,7 +1,7 @@
 export default function  ApplicationLogo(props) {
     return (
         <svg {...props} version="1.0" xmlns="http://www.w3.org/2000/svg"
-        width="400.000000pt" height="117.000000pt" viewBox="0 0 400.000000 117.000000"
+        width="400.000000pt" height="110.000000pt" viewBox="0 0 400.000000 117.000000"
         preserveAspectRatio="xMidYMid meet">
        
        <g transform="translate(0.000000,117.000000) scale(0.100000,-0.100000)"
