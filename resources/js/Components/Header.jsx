8058a83d80ca464e52bda7a3954e7fb5fde78436
Ibai <ibai.vaqueroro@elorrieta-errekamari.com>
2024-03-18 14:06:01 +0100

    header

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -93,6 +93,15 @@ const Header = ({ user }) => {
                                                 Profile
                                             </Dropdown.Link>
                                             <Dropdown.Link
+                                                href={route("category.index")}
+                                            >
+                                                Categories
+                                            </Dropdown.Link>                                            <Dropdown.Link
+                                                href={route("admin.product.index")}
+                                            >
+                                                Products
+                                            </Dropdown.Link>
+                                            <Dropdown.Link
                                                 href={route("logout")}
                                                 method="post"
                                                 as="button"
