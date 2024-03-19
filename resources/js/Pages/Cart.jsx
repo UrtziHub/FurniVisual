12d4632e6bf7e5ec7d45df6539a3efa2d7c7bc26
UrtziHub <ulusarretaga22wg@ikzubirimanteo.com>
2024-03-19 09:12:10 +0100

    Cart empty added

--- resources/js/Pages/Cart.jsx
+++ resources/js/Pages/Cart.jsx
@@ -1,24 +1,36 @@
 import CartCard from "@/Components/CartCard";
 import PageLayout from "@/Layouts/PageLayout";
 
-export default function Cart({auth, cart}) {
+export default function Cart({ auth, cart }) {
     console.log(cart);
     return (
         <PageLayout user={auth.user}>
             <div className="flex bg-gray-100 p-10 gap-2">
                 <div className="rounded flex flex-1 flex-col gap-2">
-                    {cart.products.map((product) => (
-                        <CartCard key={product.id} cart={cart} product={product}/>
-                    ))}
+                    {cart.products.length > 0 ? (
+                        cart.products.map((product) => (
+                            <CartCard
+                                key={product.id}
+                                cart={cart}
+                                product={product}
+                            />
+                        ))
+                    ) : (
+                        <p className="bg-white p-10 text-xl font-bold rounded shadow-lg">Empty cart</p>
+                    )}
                 </div>
                 <div className="bg-white p-4 flex flex-col shadow-lg rounded">
-                    <h1 className="text-2xl font-bold mb-2 underline">Summary</h1>
+                    <h1 className="text-2xl font-bold mb-2 underline">
+                        Summary
+                    </h1>
                     <h1>Delivery</h1>
                     <div>
                         <h1>Free | Express</h1>
                     </div>
                     <div className="flex-1 flex flex-col justify-end">
-                        <h1 className="text-xl border-b-2 font-bold">Cart Total</h1>
+                        <h1 className="text-xl border-b-2 font-bold">
+                            Cart Total
+                        </h1>
                         <div>
                             <div className="flex flex-col gap-1">
                                 <div className="flex justify-between">
