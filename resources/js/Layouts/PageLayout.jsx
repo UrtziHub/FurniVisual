c2f212e77e6079bc75e55ac693714a223db96ad1
UrtziHub <ulusarretaga22wg@ikzubirimanteo.com>
2024-03-21 10:08:33 +0100

    PageLayout Head

--- resources/js/Layouts/PageLayout.jsx
+++ resources/js/Layouts/PageLayout.jsx
@@ -1,9 +1,10 @@
 import Footer from "@/Components/Footer";
 import Header from "@/Components/Header";
 
-export default function PageLayout({user, children, className = ""}) {
+export default function PageLayout({user, children, className = "", headTitle}) {
     return (
         <div className={`${className}`}>
+            <Head title={headTitle}/>
             <Header user={user}/>
             {children}
             <Footer/>
